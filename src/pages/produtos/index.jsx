518de6fd01543493341fc3d7345011079815187a
Yagopatodonald <rm94746@estudante.fieb.edu.br>
2025-09-17 09:10:58 -0300

    123

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,10 +1,14 @@
-const Produtos = () => {
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-    const pizzas = [
-        'pizza 1',
-        'pizza 3',
-        'pizza 2'
-    ]
+const Produtos = () => {
+    const [pizzas, setPizzas] = useState ([])
+    axios.get("http://viacep.com.br/ws/01010000/json")
+    .then (response=>{
+        console.log(response.data)
+    })
+    .catch(error=>{console.log(error)})
+    
 
     const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
 
