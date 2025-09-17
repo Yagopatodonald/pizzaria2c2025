f56f429f53e4d25ae44a9b13163af9f2c87be927
Yagopatodonald <rm94746@estudante.fieb.edu.br>
2025-09-17 08:26:32 -0300

    lista

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,13 +1,19 @@
 const Produtos = () => {
 
+    const pizzas = [
+        'pizza 1',
+        'pizza 2',
+        'pizza 3'
+    ]
+
+    const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
+
 
     return (
         <div>
             <h3>Listagem de Produtos</h3>
             <ul>
-                <li>Pizza de Calabresa</li>
-                <li>Pizza de Muçarela</li>
-                <li>Pizza Dois Queijos</li>
+                {listaPizzas}
             </ul>
         </div>
     )
